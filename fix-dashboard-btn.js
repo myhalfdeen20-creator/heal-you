6f7c0403c3e29175487488863ec31f13765822e2
myhalfdeen20-creator <myhalfdeen20@gmail.com>
2026-09-07 14:58:34 +0700

    chore: configure deployment and remove legacy scripts
    Add GitHub Pages workflow and Netlify configuration while removing obsolete image download scripts.

--- fix-dashboard-btn.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('public/index.html', 'utf8');
-
-html = html.replace('Eksekusi Pemetaan Ulang', 'Mulai Ulang Refleksi');
-html = html.replace('Ekspor Laporan Analisis', 'Simpan Catatan Perjalanan');
-
-fs.writeFileSync('public/index.html', html);
